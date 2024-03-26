fd1faec3c62cd7bec28f2e85bb9c3181417d17d3
Neel Sadafule <neel.sadafule13@gmail.com>
2024-03-25 22:06:08 -0700

    Added research paper and data analysis scripts

---
+++ Data Anaysis/lamcsi.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+# Function to fetch Lambda and CSI data from MongoDB
+def fetch_lambda_csi_data():
+    # MongoDB connection details - replace USERNAME and PASSWORD
+    uri = "mongodb+srv://USERNAME:PASSWORD@cluster0.lxrcibg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    db = client['healthcare']
+    patients_col = db['patients']
+
+    # Fetch Lambda and CSI values for all patients
+    data = list(patients_col.find({}, {"_id": 0, "Lambda": 1, "CSI": 1}))
+
+    client.close()
+    return pd.DataFrame(data)
+
+# Fetch data
+df = fetch_lambda_csi_data()
+
+# Plotting
+plt.figure(figsize=(10, 6))
+plt.scatter(df['Lambda'], df['CSI'], alpha=0.5, c='blue', edgecolors='w', label='Patient Data')
+plt.title('Relationship between Lambda (Booking Rate) and CSI (Complexity Score Index) for Booked Appointments Only')
+plt.xlabel('Lambda (Rate of Booking Appointments per Month)')
+plt.ylabel('CSI (Complexity Score Index)')
+plt.grid(True)
+plt.show()
